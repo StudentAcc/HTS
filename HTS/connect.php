d9190b1d2aa2b1e04ca0def4b7aa1b66f22a554d
Sam-Nejad <samnejad11@gmail.com>
2021-04-10 16:48:29 +0100

    Created account-details.php and added code to entries.php and view.php to get sql

--- HTS/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Connect {
-  // A Connection is made to the local server and to the "hts" database.
-  protected function connect() {
-    $conn = new mysqli("localhost", "root", "", "hts");
-    return $conn;
-  }
-}
-?>
--- classes/view.php
+++ classes/view.php
@@ -5,21 +5,36 @@ class View extends Entries {
         $data = $this->getAllTimesheets();
         foreach ($data as $entries) {
             echo "<div class='timesheet-container'>";
-            echo "<h2>Start: ".$entries['Start']."</h2>";
-            echo "<h2>End: ".$entries['End']."</h2>";
-            echo "<p>Status: ".$entries['Status']."</p>";
-            if ($entries["Submitted"] == NULL) {
+            echo "<h2>Start: ".$entries['start']."</h2>";
+            echo "<h2>End: ".$entries['end']."</h2>";
+            echo "<p>Status: ".$entries['status']."</p>";
+            if ($entries["submitted"] == NULL) {
                 echo "<p>Date Submitted: N/A</p>";
             }
             else {
                 echo "<p>Date Submitted: ".$entries['Submitted']."</p>";
             }
-            if ($entries["Resolved"] == NULL) {
+            if ($entries["resolved"] == NULL) {
                 echo "<p>Date Resolved: N/A</p>";
             }
             else {
-                echo "<p>Date Resolved: ".$entries['Resolved']."</p>";
+                echo "<p>Date Resolved: ".$entries['resolved']."</p>";
             }
+            echo "</div>";
+        }
+    }
+
+    public function printAccountDetails() {
+        $data = $this->getAccountDetails();
+        foreach ($data as $entries) {
+            echo "<div class='timesheet-container'>";
+            echo "<h2>Account Number: ".$entries['accNumber']."</h2>";
+            echo "<p>Username: ".$entries['username']."</p>";
+            echo "<p>First Name: ".$entries['firstname']."</p>";
+            echo "<p>Surname: ".$entries['surname']."</p>";
+            echo "<p>Date of Birth: ".$entries['dateofbirth']."</p>";
+            echo "<p>Status: ".$entries['status']."</p>";
+            echo "<p>Employee ID: ".$entries['empId']."</p>";
             echo "</div>";
         }
     }
